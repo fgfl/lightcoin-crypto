b4b5a497052ff0206bf01a160fb5dfcd4ffdb651
Frederick <frederick.gfl@gmail.com>
2019-12-06 22:54:28 +0000

    Complete OOP excerise
    Made the account and transaction classes OO.

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-let balance = 500.00;
 
-class Withdrawal {
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transaction = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    if (this.transaction.length > 0) {
+      return this.transaction
+        .map(trans => trans.value)
+        .reduce((p, c) => p + c);
+    }
+    return 0;
+  }
+
+  addTransaction(transaction) {
+    this.transaction.push(transaction);
+  }
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      return true;
+    }
+    return false;
+  }
+}
+
+class Withdrawal extends Transaction {
+  isAllowed() {
+    return this.account.balance - this.amount >= 0;
   }
 
+  get value() {
+    return -1 * this.amount;
+  }
+}
+
+class Deposit extends Transaction {
+  isAllowed() {
+    return true;
+  }
+
+  get value() {
+    return this.amount;
+  }
 }
 
 
@@ -17,13 +60,18 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('snow-patrol');
 
-t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
+const t1 = new Withdrawal(50.25, myAccount);
+let commitStatus = t1.commit();
+console.log('Transaction 1:', t1, commitStatus);
 
-t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
+const t2 = new Withdrawal(9.99, myAccount);
+commitStatus = t2.commit();
+console.log('Transaction 2:', t2, commitStatus);
 
-console.log('Balance:', balance);
+const t3 = new Deposit(120.00, myAccount);
+commitStatus = t3.commit();
+console.log('Transaction 3:', t3, commitStatus);
+
+console.log('Balance:', JSON.stringify(myAccount.balance));
